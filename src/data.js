9258bb37097491a243ea60b7d0d0ee09cf7ed44c
ivan vukosavljevic <ivan.vukosavljevic99@gmail.com>
2023-01-21 02:39:24 +0100

    Inicijalni komit i dodavanje ruta

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+      id: 1,
+      title: 'ŽDREPČEVA KRV',
+      price: 450,
+      img: './images/v1.jpg',
+      desc: `Robusnog i upečatljivog imena, vino Ždrepčeva krv svoj autentičan ukus duguje unikatnoj kupaži šest pažljivo odabranih crvenih sorti vina: Vranac, Merlot, Cabernet Sauvignon, Muscat Hambourg, Frankovka i Gamay. `,
+    },
+    {
+      id: 2,
+      title: 'DI LUNA SANGRIA',
+      price:  420 ,
+      img: './images/v2.jpg',
+      desc: `Crvena Di Luna Sangria stvorena je po uzoru na čuvenu špansku sangriju, a jedinstvena kombinacija suvog voća i voćnih aroma ovo tradicionalno mediteransko vino čini idealnim izborom za ljubitelje crvenih slatkih vina.`,
+    },
+    {
+      id: 3,
+      title: 'LEDERER',
+      price: 580,
+      img: './images/v3.jpg',
+      desc: `Lederer vina nastavljaju da samouvereno  pronalaze put do onih koji odabiru vina prilaze pažljivo i promišljeno i beskompromisno osluškuju svoj lični ukus, prkoseći prolaznim trendovima svojim unikatnim stilom i snažnim karakterom. `,
+    },
+    {
+      id: 4,
+      title: 'VOĆNA VINA',
+      price: 560,
+      img: './images/v4.jpg',
+      desc: `Usled nižeg procenta alkohola i brojnih blagotvornih svojstava, voćna vina zauzimaju sve značajnije mesto u svesti onih koji uživanju pristupaju promišljeno, poklanjajući punu pažnju svom zdravlju.`,
+    },
+    {
+      id: 5,
+      title: 'ŽDREBAC',
+      price: 350,
+      img: './images/v5.jpg',
+      desc: `Ždrebac vinsku paletu čini spektar belih, crvenih i rose varijeteta vina koja volite i kojima godinama ostajete verni, a koja pored novog imena i modernog dizajna etikete, karakteriše ih unapređen ukus i konstantnost u kvalitetu. `,
+    },
+    {
+      id: 6,
+      title: 'MUŠTULUK',
+      price: 1350,
+      img: './images/v6.jpg',
+      desc: `Pitka, skladna a opet dovoljno snažna, Muštuluk vina karakteriše spoj odabranih kupaža, idealno izbalansiranih i prilagođenih najrazličitijim ukusima. `,
+    },
+    {
+      id: 7,
+      title: 'VRANAC',
+      price: 430,
+      img: './images/v7.jpg',
+      desc: `Vranac, vino koje je svojim ukusom i kvalitetom zavredilo posebno mesto u našoj vinskoj riznici. `,
+    },
+    {
+      id: 8,
+      title: 'VINARIJA OHRID-VRANAC',
+      price: 1050,
+      img: './images/v8.jpg',
+      desc: `Vranac iz Vinarije Ohrid, vino koje je svojim ukusom i kvalitetom zavredilo posebno mesto u našoj vinskoj riznici. `,
+    },
+    
+  ];
+  export default products;
