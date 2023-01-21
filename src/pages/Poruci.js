0e4afe1f50a5b6fff2ddbf97ef1b80a800fbf1fa
ivan vukosavljevic <ivan.vukosavljevic99@gmail.com>
2023-01-21 15:09:28 +0100

    "Dodavanje stranice poruci"

---
+++ src/pages/Poruci.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import items from "../data"
+function Poruci(){
+    function posalji(){
+    document.getElementById("prikaz").innerHTML=`
+        <h4>Ime: ${ime}</h4>
+        <h4>Prezime: ${prezime}</h4>
+        <h4>E-mail: ${mail}</h4>
+        <h4>Telefon: ${telefon}</h4>
+        <h4>Vino: ${vina}</h4>
+        `;   
+    
+    }
+    const [ime,setIme]=useState("");
+    const[prezime,setPrezime]=useState("");
+    const[mail,setMail]=useState("");
+    const[telefon,setTelefon]=useState("");
+    const[vina,setVina]=useState("");
+    return(
+        <div className="center">
+            <h3>Naručite vina online</h3>
+            <div id='poruciVino'>
+                <form >
+                    <input type="text" placeholder='Ime' className='razmak' onInput={(e) => { setIme(e.target.value);}} />
+                    <input type="text" placeholder='Prezime' className='razmak' onInput={(e) => { setPrezime(e.target.value);}} />
+                    <input type="email" placeholder='Mail' className='razmak' onInput={(e) => { setMail(e.target.value); }} />
+                    <input type="text" placeholder='Broj telefona' className='razmak' onInput={(e) => { setTelefon(e.target.value);}} />
+                    <select name="vina" className='razmak' onChange={(e) => { setVina(e.target.value) }} >
+                        {items.map((item) => (
+                            <option key={item.id} >{item.title}</option>
+                        ))}
+
+                    </select>
+                    <button onClick={(e) => { e.preventDefault(); posalji() }} className='razmak'>Poruči</button>
+                </form>
+            </div>
+            <h6>Naručeno:</h6>
+            <div id="prikaz">
+            </div>
+        </div>
+        
+       
+    );
+}
+export default Poruci;
